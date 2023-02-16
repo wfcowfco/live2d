284bd9ea767003532a25988440587337b6671e8e
wfcowfco <61216655+wfcowfco@users.noreply.github.com>
2023-02-16 14:52:56 +0800

    Update autoload.js

--- live2d-widget/autoload.js
+++ live2d-widget/autoload.js
@@ -1,5 +1,5 @@
 // live2d_path 参数建议使用绝对路径
-const live2d_path = "https://cdn.jsdelivr.net/gh/wfcowfco/live2d@tree/main/live2d-widget/";
+const live2d_path = "https://fastly.jsdelivr.net/gh/wfcowfco/live2d@tree/main/live2d-widget/";
 //const live2d_path = "/live2d-widget/";
 
 // 封装异步加载资源的方法
@@ -35,7 +35,7 @@ if (screen.width >= 768) {
 		initWidget({
 			waifuPath: live2d_path + "waifu-tips.json",
 			//apiPath: "https://live2d.fghrsh.net/api/",
-			cdnPath: "https://cdn.jsdelivr.net/gh/wfcowfco/live2d@tree/master/live2d-api/",
+			cdnPath: "https://fastly.jsdelivr.net/gh/wfcowfco/live2d@tree/master/live2d-api/",
 			tools: ["hitokoto", "asteroids", "switch-model", "switch-texture", "photo", "info", "quit"]
 		});
 	});
